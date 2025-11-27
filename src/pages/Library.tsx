c0a5f9185f074943282fd6f820dabe089cbf83a5
Gropaa <support@gropaa.in>
2025-11-27 14:01:37 +0530

    BookCardf Adjusted

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -187,7 +187,7 @@ const Library = () => {
             </p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {filteredBooks.map(book => (
               <BookCard key={book.id} book={book} />
             ))}
